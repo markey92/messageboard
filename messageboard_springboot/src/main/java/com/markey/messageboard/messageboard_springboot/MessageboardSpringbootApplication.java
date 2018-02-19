85504b4d01e51545c7939e5d71cb148bb65a8674
markey92 <1053667066@qq.com>
2018-02-19 10:16:28 +0800

    init

---
+++ messageboard_springboot/src/main/java/com/markey/messageboard/messageboard_springboot/MessageboardSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.markey.messageboard.messageboard_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageboardSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageboardSpringbootApplication.class, args);
+	}
+}
